517ff21fd9951d138f03885b6637bc66fa7e58ec
Nicolas Inoue <144853890+Nic-Inoue@users.noreply.github.com>
2024-09-05 17:33:18 -0300

    Uploading all code

---
+++ imersão/dados.js
@@ -0,0 +1,47 @@
+let dados = [
+    {
+        titulo: "Finn, o Humano",
+        especie: "Humano",
+        ocupacao: "Aventureiro",
+        descricao: "Finn é um humano, protagonista da série. É um aventureiro otimista e corajoso, sempre pronto para ajudar seus amigos. Apesar de sua juventude, ele demonstra grande maturidade e compaixão.",
+        link: "https://horadeaventura.fandom.com/pt-br/wiki/Finn"
+    },
+    {
+        titulo: "Jake, o Cachorro",
+        especie: "Cão Mágico",
+        ocupacao: "Aventureiro, Melhor Amigo",
+        descricao: "Jake é um cão mágico e o melhor amigo de Finn. Com a habilidade de mudar de forma e tamanho, ele traz uma dose de comédia e flexibilidade para as aventuras na Terra de Ooo. Apesar de sua natureza brincalhona, Jake é um companheiro leal e protetor.",
+        link: "https://horadeaventura.fandom.com/pt-br/wiki/Jake"
+    },
+    {
+        titulo: "Marceline, a Rainha dos Vampiros",
+        especie: "Vampira, Demônio",
+        ocupacao: "Música, Exploradora",
+        descricao: "Marceline é uma vampira milenar com um coração gentil. Apesar de sua aparência sombria e amor por música, ela é uma amiga leal e protetora de Finn e Jake. Seu passado misterioso a torna um personagem intrigante.",
+        link: "https://horadeaventura.fandom.com/pt-br/wiki/Marceline"
+    },
+    {
+        titulo: "Hunson Abadeer",
+        especie: "Demônio",
+        ocupacao: "Governante",
+        descricao: "Hunson Abadeer é um demônio milenar e pai da Marceline. Ele é conhecido por sua personalidade egoísta e interesse em dinheiro. Apesar de sua natureza, ele demonstra um lado mais sentimental em relação à sua filha.",
+        link: "https://horadeaventura.fandom.com/pt-br/wiki/Hunson_Abadeer",
+        habitacao: "Noitosfera"
+    },
+    {
+        titulo: "Gunter",
+        especie: "Pinguim, Entidade Cósmica",
+        ocupacao: "Mascote",
+        descricao: "Gunter é um pinguim falante e mascote do Rei Gelado. À primeira vista, pode parecer apenas um simples pinguim de estimação do Rei Gelado, mas suas aparições revelam uma complexidade que o torna um dos personagens mais intrigantes da série.",
+        link: "https://horadeaventura.fandom.com/pt-br/wiki/Gunter",
+        habitacao: "Reino de gelo"
+    },
+    {
+        titulo: "Lich",
+        especie: "Entidade Cósmica",
+        ocupacao: "Ultimo discípulo de GOLB",
+        descricao: "O Lich é um ser antigo e poderoso, reconhecido como a personificação do mal, que tem como objetivo destruir toda a vida no Multiverso.. Ele é um dos vilões mais perigosos da série.",
+        link: "https://horadeaventura.fandom.com/pt-br/wiki/Lich",
+        habitacao: "Dimensão do Lich"
+    }
+]
