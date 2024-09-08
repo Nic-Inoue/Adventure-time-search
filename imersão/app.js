7c74ffbff9cd80eddaa028869c1eff6323544ee2
Nicolas Inoue <144853890+Nic-Inoue@users.noreply.github.com>
2024-09-07 23:44:22 -0300

    Upload
    background and characters pictures and code improvements

--- imersão/app.js
+++ imersão/app.js
@@ -1,20 +1,121 @@
-function pesquisar() {
-    let section = document.getElementById("resultados-pesquisa");
-    console.log(section);
+let section = document.getElementById("resultados-pesquisa");
+const main = document.getElementById("main");
+const imageLogo = document.getElementById("imageLogo");
+const filtros = document.getElementById("filtros"); // seleção de toda seção apenas para animação
+const botoesFiltro = document.querySelectorAll(".filtro-opcao");
+const body = document.getElementById("body");
+const footer = document.getElementById("footer");
 
-    let resultados = ""
+let resultados = "";
+
+function pesquisar() {
+    imageLogo.style.width = "0rem";
+    imageLogo.style.height = "0rem";
+    imageLogo.style.opacity = "0"
+    main.style.margin = "0";
+    section.style.height = "50vh";
+    filtros.style.opacity = "1";
+    filtros.style.height = "auto";
+    let resultados = "";
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    if (campoPesquisa == "") {
+        section.innerHTML = "<p>Ninguém foi encontrado. </p>"
+        return;
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     for (let dado of dados) {
-        resultados += `
-    <div class="item-resultado">
-        <h2>
-            <a href="#" target="_blank">${dado.titulo}</a>
-        </h2>
-        <p class="descricao-meta">${dado.descricao}</p>
-        <a href="${dado.link}" target="_blank">Mais informações</a>
-    </div>
-`
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            resultados += `
+            <div class="item-resultado" id="item-resultado">
+                <img src="${dado.imagem}" alt="Foto do ${dado.titulo}">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <h3>
+                    ${dado.especie} - ${dado.ocupacao}
+                </h3>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href="${dado.link}" target="_blank">Mais informações</a>
+            </div>
+            `
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Ninguém foi encontrado. Escreva o nome de algum personagem!</p>"
     }
     section.innerHTML = resultados
 }
 
+function filtrar(filtroNome, filtroTipo){
+    resultados = "";
+    section.innerHTML = "";
+    for (let dado of dados) {
+        let filtroNomeMinusculo = filtroNome.toLowerCase();
+        let especieMinusculo = dado[filtroTipo].toLowerCase();
+
+        if (especieMinusculo.includes(filtroNomeMinusculo)){
+                resultados += `
+            <div class="item-resultado" id="item-resultado">
+                <img src="${dado.imagem}" alt="Foto do ${dado.titulo}">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <h3>
+                    ${dado.especie} - ${dado.ocupacao}
+                </h3>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href="${dado.link}" target="_blank">Mais informações</a>
+            </div>
+            `
+        }
+    }
+    section.innerHTML = resultados;
+    const itemResultados = document.getElementsByClassName("item-resultado");
+    console.log(itemResultados);
+            if (filtroTipo.includes("regiao")){
+                if(filtroNome.includes("Casa da Árvore")){
+                    for(let item of itemResultados){
+                        item.style.background = "#04E762";
+                    }
+                    body.style.backgroundImage = "url(./assets/treehouse.jpg)";
+                    footer.style.background = "#008BF8"
+                }
+                if(filtroNome.includes("Noitosfera")){
+                    for(let item of itemResultados){
+                        item.style.background = "#FF7881";
+                    }
+                    body.style.backgroundImage = "url(./assets/noitosphere.jpg)";
+                    footer.style.background = "#DB0021"
+                }
+                if(filtroNome.includes("Reino do Gelo")){
+                    for(let item of itemResultados){
+                        item.style.background = "#00BADB";
+                    }
+                    body.style.backgroundImage = "url(./assets/ice-kingdom.jpg)";
+                    footer.style.background = "#008BF8"
+                }
+                if(filtroNome.includes("Reino Doce")){
+                    for(let item of itemResultados){
+                        item.style.background = "#F34CA2";
+                    }
+                    body.style.backgroundImage = "url(./assets/candy-kingdom.jpg)";
+                    footer.style.background = "#DC0073"
+                }
+                if(filtroNome.includes("Reino do Fogo")){
+                    for(let item of itemResultados){
+                        item.style.background = "#F5BB12";
+                    }
+                    body.style.backgroundImage = "url(./assets/fire-kingdom.jpg)";
+                    footer.style.background = "#DB0021"
+                }
+            }
+}
--- imersão/dados.js
+++ imersão/dados.js
@@ -1,47 +1,82 @@
 let dados = [
     {
-        titulo: "Finn, o Humano",
+        imagem: "./assets/finn.jpg",
+        titulo: "Finn",
         especie: "Humano",
         ocupacao: "Aventureiro",
         descricao: "Finn é um humano, protagonista da série. É um aventureiro otimista e corajoso, sempre pronto para ajudar seus amigos. Apesar de sua juventude, ele demonstra grande maturidade e compaixão.",
-        link: "https://horadeaventura.fandom.com/pt-br/wiki/Finn"
+        link: "https://horadeaventura.fandom.com/pt-br/wiki/wiki/Finn",
+        regiao: "Casa da Árvore",
+        tags: "humano aventureiro otimista corajoso amigo jovem maturo compassivo"
     },
     {
-        titulo: "Jake, o Cachorro",
+        imagem: "./assets/jake.jpg",
+        titulo: "Jake",
         especie: "Cão Mágico",
-        ocupacao: "Aventureiro, Melhor Amigo",
-        descricao: "Jake é um cão mágico e o melhor amigo de Finn. Com a habilidade de mudar de forma e tamanho, ele traz uma dose de comédia e flexibilidade para as aventuras na Terra de Ooo. Apesar de sua natureza brincalhona, Jake é um companheiro leal e protetor.",
-        link: "https://horadeaventura.fandom.com/pt-br/wiki/Jake"
+        ocupacao: "Aventureiro",
+        descricao: "Jake é um cão mágico e o melhor amigo de Finn. Com a habilidade de mudar de forma e tamanho, ele traz uma dose de comédia para as aventuras na Terra de Ooo. Apesar de brincalhão, é um companheiro leal e protetor.",
+        link: "https://horadeaventura.fandom.com/pt-br/wiki/Jake",
+        regiao: "Casa da Árvore",
+        tags: "cão mágico aventureiro brincalhão leal protetor flexível cômico"
     },
     {
-        titulo: "Marceline, a Rainha dos Vampiros",
-        especie: "Vampira, Demônio",
-        ocupacao: "Música, Exploradora",
-        descricao: "Marceline é uma vampira milenar com um coração gentil. Apesar de sua aparência sombria e amor por música, ela é uma amiga leal e protetora de Finn e Jake. Seu passado misterioso a torna um personagem intrigante.",
-        link: "https://horadeaventura.fandom.com/pt-br/wiki/Marceline"
+        imagem: "./assets/marceline.jpg",
+        titulo: "Marceline",
+        especie: "Vampiro, demônio",
+        ocupacao: "Músico, aventureiro",
+        descricao: "Marceline é uma vampira milenar. Ela é uma amiga leal e protetora de Finn e Jake. Seu passado misterioso a torna um personagem intrigante.",
+        link: "https://horadeaventura.fandom.com/pt-br/wiki/Marceline",
+        regiao: "Noitosfera",
+        tags: "vampiro demônio músico aventureiro leal protetora misteriosa passado"
     },
     {
+        imagem: "./assets/hunson abadeer.jpg",
         titulo: "Hunson Abadeer",
-        especie: "Demônio",
+        especie: "Demônio, vampiro",
         ocupacao: "Governante",
-        descricao: "Hunson Abadeer é um demônio milenar e pai da Marceline. Ele é conhecido por sua personalidade egoísta e interesse em dinheiro. Apesar de sua natureza, ele demonstra um lado mais sentimental em relação à sua filha.",
+        descricao: "Hunson Abadeer é um demônio. Ele é conhecido por sua personalidade egoísta. Apesar de sua natureza, ele demonstra um lado mais sentimental em relação à sua filha, Marceline.",
         link: "https://horadeaventura.fandom.com/pt-br/wiki/Hunson_Abadeer",
-        habitacao: "Noitosfera"
+        regiao: "Noitosfera",
+        tags: "demônio governante egoísta cruel sentimental pai"
     },
     {
+        imagem: "./assets/gunter.jpg",
         titulo: "Gunter",
-        especie: "Pinguim, Entidade Cósmica",
+        especie: "Pinguim",
         ocupacao: "Mascote",
-        descricao: "Gunter é um pinguim falante e mascote do Rei Gelado. À primeira vista, pode parecer apenas um simples pinguim de estimação do Rei Gelado, mas suas aparições revelam uma complexidade que o torna um dos personagens mais intrigantes da série.",
+        descricao: "Gunter é um pinguim falante e mascote do Rei Gelado. À primeira vista, pode parecer apenas um simples pinguim de estimação do Rei Gelado, mas suas aparições revelam uma complexidade interessante.",
         link: "https://horadeaventura.fandom.com/pt-br/wiki/Gunter",
-        habitacao: "Reino de gelo"
+        regiao: "Reino do Gelo",
+        tags: "pinguim fofo mascote misterioso vilão poderoso"
     },
     {
-        titulo: "Lich",
-        especie: "Entidade Cósmica",
-        ocupacao: "Ultimo discípulo de GOLB",
-        descricao: "O Lich é um ser antigo e poderoso, reconhecido como a personificação do mal, que tem como objetivo destruir toda a vida no Multiverso.. Ele é um dos vilões mais perigosos da série.",
-        link: "https://horadeaventura.fandom.com/pt-br/wiki/Lich",
-        habitacao: "Dimensão do Lich"
+        imagem: "./assets/rei gelado.jpg",
+        titulo: "Rei Gelado",
+        especie: "Elemental",
+        ocupacao: "Governante",
+        descricao: "O Rei Gelado é um antigo mago humano que se transformou em um ser imortal e obsesso por princesas. Ele é o governante do Reino do Gelo e um dos principais antagonistas da série.",
+        link: "https://horadeaventura.fandom.com/pt-br/wiki/Rei Gelado",
+        regiao: "Reino do Gelo",
+        tags: "mago imortal obsessão governante antagonista triste"
+    },
+    {
+        imagem: "./assets/princesa jujuba.jpg",
+        titulo: "Princesa Jujuba",
+        especie: "Elemental",
+        ocupacao: "Governante",
+        descricao: "A Princesa Jujuba é uma cientista brilhante e a governante do Reino Doce. Ela é conhecida por sua inteligência e paixão pela ciência.",
+        link: "https://horadeaventura.fandom.com/pt-br/wiki/Princesa_Jujuba",
+        regiao: "Reino Doce",
+        tags: "cientista governante inteligente doce paixão ciência"
+    },
+    {
+        imagem: "./assets/princesa de fogo.jpg",
+        titulo: "Princesa de Fogo",
+        especie: "Elemental",
+        ocupacao: "Governante",
+        descricao: "A Princesa de Fogo é a governante do Reino do Fogo. Ela é conhecida por seu temperamento forte e sua paixão por fogueiras.",
+        link: "https://horadeaventura.fandom.com/pt-br/wiki/Princesa_de_Fogo",
+        regiao: "Reino do Fogo",
+        tags: "fogo governante temperamento paixão poderosa"
     }
-]
+];
